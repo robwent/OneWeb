f74635fef953565160627f5a3f4fbd8a7ae59c82
Seth Warburton <seth@internet-inspired.com>
2012-01-02 16:41:40 +0000

    refined the resize menuswitch event

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 Seth Warburton | @nternetinspired
 */
 
-$(document).ready(function(){
+jQuery(document).ready(function(){
 
 /* Mobile Menu - Seth Warburton @nternetinspired */
 	jQuery("nav#menu").addClass('visuallyhidden');
@@ -10,28 +10,21 @@ $(document).ready(function(){
 	event.preventDefault();
 	jQuery('nav#menu').toggleClass('visuallyhidden');
 });	
-	function adjustStyle(width) {
-    width = parseInt(width);
-    if (width > 767) {
+
+	function menuSwitch(width) {
+    width = jQuery(window).width();
+    if (width > 720){
 		jQuery('nav#menu').removeClass('visuallyhidden');
     }
 }
+menuSwitch();
 	// Chris Coyier's elegant screensize test http://css-tricks.com/resolution-specific-stylesheets/
 	jQuery(function() {
-    adjustStyle($(this).width());
+    menuSwitch(jQuery(window).width());
     jQuery(window).resize(function() {
-        adjustStyle($(this).width());
+        menuSwitch(jQuery(window).width());
     });
 });	
-
-	// Run Matt Kersley's jQuery Responsive menu plugin (see plugins.js)
-	if (jQuery.fn.mobileMenu) {
-		jQuery('ol#id').mobileMenu({
-			switchWidth: 768,                   // width (in px to switch at)
-			topOptionText: 'Choose a page',     // first option text
-			indentString: '&nbsp;&nbsp;&nbsp;'  // string for indenting nested items
-		});
-	}
 
 	// Run Mathias Bynens jQuery placeholder plugin (see plugins.js)
 	if (jQuery.fn.placeholder) {
