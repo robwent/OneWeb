0bd63415265bf7f1482c6dbf575d398777bc255e
Seth Warburton <seth@internet-inspired.com>
2012-01-04 11:45:34 +0000

    updated version number

--- functions/logic.php
+++ functions/logic.php
@@ -2,7 +2,7 @@
 /* =====================================================================
 Template:	OneWeb for Joomla 1.7						            
 Author: 	Seth Warburton - Internet Inspired! - @nternetinspired 				            
-Version: 	0.1 											             
+Version: 	0.2 											             
 Created: 	Dec 2011                                                    
 Copyright:	Seth Warburton - (C) 2011 - All rights reserved
 Sources:	http://construct-framework.com/				         					         
