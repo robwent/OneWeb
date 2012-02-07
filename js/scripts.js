c16c114fa89e21733979c29b7931718104b20979
Seth Warburton <seth@internet-inspired.com>
2012-02-07 11:51:54 +0000

    cleaned up the jQuery a bit

--- js/scripts.js
+++ js/scripts.js
@@ -4,31 +4,34 @@ Seth Warburton | @nternetinspired
 
 jQuery(document).ready(function(){
 
-/* Mobile Menu - Seth Warburton @nternetinspired */
-	jQuery("nav#menu").addClass('visuallyhidden');
-	jQuery("a#mobileMenu").click(function(event) {	
-	event.preventDefault();
-	jQuery('nav#menu').toggleClass('visuallyhidden');
-});	
-
-	function menuSwitch(width) {
-    width = jQuery(window).width();
-    if (width > 720){
-		jQuery('nav#menu').removeClass('visuallyhidden');
-    }
+// Run Mathias Bynens jQuery placeholder plugin (see plugins.js)
+if (jQuery.fn.placeholder) {
+	jQuery('input, textarea').placeholder();
 }
+
+/* Mobile Menu - Seth Warburton @nternetinspired */
+var menu = jQuery('#menu')
+
+menu.addClass('visuallyhidden');
+jQuery("a#mobileMenu").click(function(event) {
+event.preventDefault();
+menu.toggleClass('visuallyhidden');
+											});	
+function menuSwitch(width) {
+	width = jQuery(window).width();
+	if (width > 760){
+		menu.removeClass('visuallyhidden');
+					}
+		else menu.addClass('visuallyhidden');
+							}
 menuSwitch();
-	// Chris Coyier's elegant screensize test http://css-tricks.com/resolution-specific-stylesheets/
+
+/* Chris Coyier's elegant screensize test http://css-tricks.com/resolution-specific-stylesheets/ */
 	jQuery(function() {
     menuSwitch(jQuery(window).width());
     jQuery(window).resize(function() {
         menuSwitch(jQuery(window).width());
     });
 });	
-
-	// Run Mathias Bynens jQuery placeholder plugin (see plugins.js)
-	if (jQuery.fn.placeholder) {
-		jQuery('input, textarea').placeholder();		
-	}
 
 });
